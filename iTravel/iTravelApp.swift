530fdcfda80358fd9c38cea3eecd0796869d4736
Elfana, A | Elfaael <elfaael12@gmail.com>
2024-04-09 08:55:48 +0700

    Initial Commit

---
+++ iTravel/iTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTravelApp.swift
+//  iTravel
+//
+//  Created by Elfana Anamta Chatya on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct iTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
